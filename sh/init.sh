440e6c8efe6c310f1f658f09398cd2aefa1883a2
Sumit Chohan <chauhansumit@gmail.com>
2018-05-28 00:47:37 -0500

    df

--- sh/init.sh
+++ sh/init.sh
@@ -1,5 +1,31 @@
+#cd /sdcard/coc
+cd /c/work/GH/sumitchohan.github.io/sh
+
+echo %1
+
 Greet()
 {
 	echo "Hello {0}!"
+} 
+echo
+
+loop()
+{
+	retryIndex=1
+	# retryCount=10000000
+	retryCount=10
+	retryDelay=2
+	error="y"
+	while [ $retryIndex -le $retryCount ]
+	do 
+	echo "index - $retryIndex";
+	sleep $retryDelay	
+	(( retryIndex++ ))
+	if [ -e break.txt ]
+	then
+		break
+	fi
+	done 
 }
-echo "done!"
+loop & pid=$!
+echo $pid 
--- sh/run.sh
+++ sh/run.sh
@@ -2,5 +2,5 @@ cd ~
 mkdir tmp
 cd tmp
 curl -O https://raw.githubusercontent.com/sumitchohan/sumitchohan.github.io/master/sh/init.sh
-adb push ~\tmp\init.sh \sdcard\coc\init.sh
-adb shell "sh \sdcard\coc\init.sh"
+adb push ~/tmp/init.sh /sdcard/coc/init.sh
+adb shell "sh /sdcard/coc/init.sh"
