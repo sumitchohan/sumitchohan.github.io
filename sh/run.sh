e501bc9240bcae37f6f36d537c5461bcd3585bd1
Sumit Chohan <chauhansumit@gmail.com>
2018-05-27 23:31:43 -0500

    tt

---
+++ sh/run.sh
@@ -0,0 +1,6 @@
+cd ~
+mkdir tmp
+cd tmp
+curl -O https://raw.githubusercontent.com/sumitchohan/sumitchohan.github.io/master/sh/init.sh
+adb push ~\tmp\init.sh \sdcard\coc\init.sh
+adb shell "sh \sdcard\coc\init.sh"
