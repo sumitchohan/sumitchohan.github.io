fffd320fe48faabda3a94969f473bc829cff1c95
sumitchohan <chauhansumit@gmail.com>
2019-01-14 10:04:15 +0530

    Update run.sh

--- sh/run.sh
+++ sh/run.sh
@@ -1,1 +1,9 @@
-LogRemote "resetting" && source do.sh && Start
+Read "Home"
+
+		trophy=$(cat ocred_Trophy.txt)
+		de=$(cat ocred_DE.txt)
+		gold=$(cat ocred_Gold.txt)
+		elixir=$(cat ocred_Elixir.txt)
+		gems=$(cat ocred_Gems.txt)
+
+LogRemote "Trophy - $trophy Gold - $gold Elixir - $elixir DE - $de Gems - $gems"
