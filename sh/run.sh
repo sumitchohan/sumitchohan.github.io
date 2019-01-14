5e999edb14edc2dfb7078cd6cff176981d9300fd
sumitchohan <chauhansumit@gmail.com>
2019-01-14 10:12:07 +0530

    Update run.sh

--- sh/run.sh
+++ sh/run.sh
@@ -1,4 +1,12 @@
-LogRemote "FILE"
+LogRemote "FILE_1"
+
+echo "Trophy,674,82,23,69,255_255_255">Home.config
+echo "Gold,749,1073,24,138,255_255_255">>Home.config
+echo "Elixir,686,1082,26,132,255_255_255">>Home.config
+echo "DE,624,1101,26,115,255_255_255">>Home.config
+echo "Gems,565,1132,26,82,255_255_255">>Home.config
+
+
 Read "Home"
 
 		trophy=$(cat ocred_Trophy.txt)
