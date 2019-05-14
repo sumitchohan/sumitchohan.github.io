206fbc7e5fe2db4777f5761e14405eb945fc6c9d
sumit <sumit@sumits-MacBook-Pro.local>
2019-05-14 08:56:22 +0530

    wip

--- sh/run.sh
+++ sh/run.sh
@@ -1,15 +1,15 @@
 LogRemote "FILE_5"
-rm log*
-rm error* 
-rm *.png
-cd ..
-rm log*
-rm error*
-rm *.png 
-cd coc
-LogRemote "Cleared"
-#curl -s -k https://raw.githubusercontent.com/sumitchohan/AndroidAutoSh/master/update.sh -o update.sh
-#source update.sh
+#rm log*
+#rm error* 
+#rm *.png
+#cd ..
+#rm log*
+#rm error*
+#rm *.png 
+#cd coc
+#LogRemote "Cleared"
+curl -s -k https://raw.githubusercontent.com/sumitchohan/AndroidAutoSh/master/update.sh -o update.sh
+source update.sh
 
 waitCount=15
 svc power stayon true
