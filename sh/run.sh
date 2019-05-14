2fe5cbe26ed1636e5eab5c19fd75792cb01a96b0
sumit <sumit@sumits-MacBook-Pro.local>
2019-05-14 10:57:46 +0530

    wip

--- sh/run.sh
+++ sh/run.sh
@@ -10,7 +10,16 @@ LogRemote "FILE_5"
 #LogRemote "Cleared"
 curl -s -k https://raw.githubusercontent.com/sumitchohan/AndroidAutoSh/master/update.sh -o update.sh
 source update.sh
+rm ocred_DE.txt
+rm ocred_Elixir.txt
+rm ocred_Gold.txt
 ReadBattleTest
+
+				de=$(cat ocred_DE.txt)
+				elixir=$(cat ocred_Elixir.txt)
+				gold=$(cat ocred_Gold.txt)  
+				#isth10=$(echo $th10| cut -d'_' -f 1)
+				LogRemote "elixir - $elixir , gold - $gold , de - $de"
 waitCount=15
 svc power stayon true
 
