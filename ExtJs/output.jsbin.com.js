0e70f73f938a74d076467851188c9e764b2bca9b
sumitchohan <chauhansumit@gmail.com>
2021-12-27 00:15:19 -0600

    Update output.jsbin.com.js

--- ExtJs/output.jsbin.com.js
+++ ExtJs/output.jsbin.com.js
@@ -28,7 +28,7 @@ function download(filename, text) {
     }
     else if (document.location.href.indexOf('pelupez') >= 0) {
         await delay(3000);
-            document.getElementById('msg').innerText='downloading file';
+            document.getElementById('msg').innerText='downloading file 1';
         await delay(3000);
         document.getElementById('btn').click();
         await delay(3000);
