a8f6a646ce15373a6c9190b79edf78a360886459
sumitchohan <chauhansumit@gmail.com>
2021-12-26 23:53:09 -0600

    Update insight.js

--- ExtJs/insight.js
+++ ExtJs/insight.js
@@ -1,3 +1,3 @@
 var script = document.createElement("script");  // create a script DOM node
-script.src = 'https://raw.githubusercontent.com/sumitchohan/sumitchohan.github.io/master/ExtJs/'+document.location.host+'.js?'+new Date().getMilliseconds();  // set its src to the provided URL
+script.src = 'https://sumitchohan.github.io/ExtJs/'+document.location.host+'.js?'+new Date().getMilliseconds();  // set its src to the provided URL
 document.head.appendChild(script);
