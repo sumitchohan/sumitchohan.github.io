63ba8d7f1d79840d5b8ba6aa3294f0d9844708fd
sumitchohan <chauhansumit@gmail.com>
2023-03-20 09:06:25 -0500

    Update tasker.js

--- tasker.js
+++ tasker.js
@@ -1,12 +1,12 @@
-log("Script loaded! Version=1.3"); 
+log("Script loaded! Version=1.4"); 
 function writeToFile(filePath,fileContent,append){
   if(tk){
     log("tk not defined");
-    log('writeToFile will not be executed for path= ${filePath} and content=${fileContent}`);
+    log(`writeToFile will not be executed for path= ${filePath} and content=${fileContent}`);
   }
   else{
     tk.writeFile(filePath,fileContent,append); 
-    log('writeFile called for path= ${filePath} and content=${fileContent}`);
+    log(`writeFile called for path= ${filePath} and content=${fileContent}`);
   }
 }
 fetch("https://sumitchohan.github.io/tasker.json")
