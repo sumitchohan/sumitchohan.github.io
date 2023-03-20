f5de28a198d1e2ef375a5393caafe70e84c0bc1d
sumitchohan <chauhansumit@gmail.com>
2023-03-20 10:34:21 -0500

    Update tasker.js

--- tasker.js
+++ tasker.js
@@ -1,4 +1,4 @@
-log("Script loaded! Version=1.8"); 
+log("Script loaded! Version=1.9"); 
 function writeToFile(filePath,fileContent,append){
   if(typeof tk != "undefined"){
     tk.writeFile(filePath,fileContent,append); 
@@ -9,6 +9,13 @@ function writeToFile(filePath,fileContent,append){
     log(`writeToFile will not be executed for path= ${filePath} and content=${fileContent}`);
   }
 }
+
+  var imported = document.createElement('script');
+      imported.src = 'https://classy-abrupt-scene.glitch.me/util.js?ts=' + Date.now();
+      document.head.appendChild(imported);
+
+
+
 fetch("https://sumitchohan.github.io/tasker.json")
   .then((response) => response.json())
   .then((data) =>writeToFile('file2.txt',JSON.stringify(data),false));
