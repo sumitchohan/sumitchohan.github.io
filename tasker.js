7141b166b5c5950257ae6db33b503fbf203fd85d
sumitchohan <chauhansumit@gmail.com>
2023-03-20 08:18:28 -0500

    Update tasker.js

--- tasker.js
+++ tasker.js
@@ -1,2 +1,6 @@
-alert('hello from gh 3');
+const para = document.createElement("p");
+const node = document.createTextNode("Script loaded! Version=1.0");
+para.appendChild(node);
+const element = document.getElementById("div1");
+element.appendChild(para);
 tk.writeFile('file2.txt','Hello there',false)
