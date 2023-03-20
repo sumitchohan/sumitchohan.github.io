dee9ce37dedf977dc81fdd01841748d26a0b05c0
sumitchohan <chauhansumit@gmail.com>
2023-03-20 08:50:21 -0500

    Update tasker.js

--- tasker.js
+++ tasker.js
@@ -1,8 +1,8 @@
 const para = document.createElement("p");
-const node = document.createTextNode("Script loaded! Version=1.1");
+const node = document.createTextNode("Script loaded! Version=1.2");
 para.appendChild(node);
 const element = document.getElementById("div1");
 element.appendChild(para);
 fetch("https://sumitchohan.github.io/tasker.json")
   .then((response) => response.json())
-  .then((data) =>tk.writeFile('file2.txt',data,false));
+  .then((data) =>tk.writeFile('file2.txt',JSON.stringify(data),false));
