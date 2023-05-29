ee02ef700d893f189c9e33d0ec05c0bfefa4cfbe
Sumit Chohan <Sumit.Chohan@ttx.com>
2023-05-29 12:40:22 -0500

    wip

--- ExtJs/docs.google.com.js
+++ ExtJs/docs.google.com.js
@@ -1,18 +1,1 @@
-
-function delay(ms) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, ms);
-    });
-}
-(async () => {
-    await delay(10000);
-    var elements = document.getElementsByTagName("div");
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].innerHTML === "KeyValue") {
-            console.log(elements[i].outerHTML)
-            elements[i].remove();
-        }
-    }
-})();
-
-
+setTimeout(function(){  alert('hello');},3000)
