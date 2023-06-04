628b31f5866f1874399193e50cd1cb02efa5cb58
sumitchohan <chauhansumit@gmail.com>
2023-06-03 23:45:32 -0500

    Update docs.google.com.js

--- ExtJs/docs.google.com.js
+++ ExtJs/docs.google.com.js
@@ -1,9 +1,1 @@
-try {
-    document.body.style.border = "5px solid green";
-    var script = document.createElement("script");  // create a script DOM node
-    script.src = 'https://blueninja.glitch.me/docs.google.com.js?' + new Date().getMilliseconds();
-    script.id = "ExtJs1"
-    document.head.appendChild(script);
-  } catch (error) { 
-  }
-  
+
