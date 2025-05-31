6230ce39adaedbe9779cfa55a76386e30046de53
sumitchohan <chauhansumit@gmail.com>
2025-05-31 13:00:08 -0400

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,3 +1,3 @@
-#pip install azure-identity requests
-#curl -o start_app.py https://sumitchohan.github.io/start_app.py && python start_app.py
-echo '<h1>Token Retrieved. Server Running.</h1>' > index.html && python -m http.server 8080
+pip install azure-identity requests
+curl -o start_app.py https://sumitchohan.github.io/start_app.py && python start_app.py
+echo '<h1>Hello Therre! 1</h1>' > index.html && python -m http.server 8080
